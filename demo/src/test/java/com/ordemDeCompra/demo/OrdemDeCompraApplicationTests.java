f99b90f4b01b5db8286dad30665091dca78b5986
Gabriel Della Giustina <gabrielsdgiustina@gmail.com>
2024-05-22 16:35:50 -0300

    base

---
+++ demo/src/test/java/com/ordemDeCompra/demo/OrdemDeCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordemDeCompra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemDeCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
