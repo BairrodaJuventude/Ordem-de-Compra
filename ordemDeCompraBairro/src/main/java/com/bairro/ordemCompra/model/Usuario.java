88005a513867b313072270ae116090b4634867f8
Gabriel Della Giustina <gabrielsdgiustina@gmail.com>
2024-05-23 16:49:16 -0300

    Base final
    A partir daqui commitem em branches próprias.

---
+++ ordemDeCompraBairro/src/main/java/com/bairro/ordemCompra/model/Usuario.java
@@ -0,0 +1,16 @@
+package com.bairro.ordemCompra.model;
+
+public class Usuario {
+    private String usuario;
+
+    private String email;
+
+    private String password;
+
+    private String token;
+
+    private Boolean admin;
+}
+
+//region getters e setters
+
